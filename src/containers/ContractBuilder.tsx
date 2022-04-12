66a77318af9deea7db8a05450e4e157736d471d0
eoj <joeamos17@gmail.com>
2022-04-12 11:35:23 -0700

    dropdown and enpoint state local state functionality added. save contract logic started

--- src/containers/ContractBuilder.tsx
+++ src/containers/ContractBuilder.tsx
@@ -8,11 +8,24 @@ import ContractEditor from '../components/ContractEditor';
 export default function ContractBuilder() {
 
   const [reqMethod, setReqMethod] = useState('GET')
+  const [endpoint, setEndpoint] = useState('')
 
-  // const [reqBody, setReqBody] = useState({})
+  const handleSetReqMethod = (e: any): void => {
+    const method:string = e.target.value
+    console.log("method changed: ", method)
+    setReqMethod(method);
+  }
+
+  const handleSetEndpoint = (e: any): void => {
+    const endpoint: string = e.target.value;
+    console.log("current endpoint string: ", e.target.value);
+    setEndpoint(endpoint)
+  }
+
+  
   return (
     <div className='bg-gray-900'>
-      <ContractEndpoint reqMethod={reqMethod} setReqMethod={setReqMethod}/>
+      <ContractEndpoint reqMethod={reqMethod} setReqMethod={handleSetReqMethod} endpoint={endpoint} setEndpoint={handleSetEndpoint}/>
       <ContractEditor reqMethod={reqMethod}/>
      {/* <div className="request-specification-container">
        <div className="request-method">Request type</div>
