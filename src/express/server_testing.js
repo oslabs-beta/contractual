c05ff884e66f66e27ce753cc2894321efaac9dd0
Yankun Song <yankun.l.song@gmail.com>
2022-04-14 13:46:16 -0500

    failed to use websocket

--- src/express/server_testing.js
+++ src/express/server_testing.js
@@ -1,57 +1,63 @@
-const express2 = require("express");
-const app2 = express2();
-const path2 = require("path");
+// const express2 = require("express");
+// const app2 = express2();
+// const path2 = require("path");
 
-const PORT2 = process.env.PORT || 1234; // setting the port
-
-// const socketIO = require("socket.io");
-// const http = require("http");
 // const PORT2 = process.env.PORT || 1234; // setting the port
-// let server = http.createServer(app2);
-// let io = socketIO(server);
-
-// // make connection with user from server side
-// io.on("connection", (socket) => {
-//   console.log("New user connected");
-//   //emit message from server to user
-//   socket.emit("newMessage", {
-//     from: "jen@mds",
-//     text: "hepppp",
-//     createdAt: 123,
-//   });
-
-//   // listen for message from user
-//   socket.on("createMessage", (newMessage) => {
-//     console.log("newMessage", newMessage);
-//   });
-
-//   // when server disconnects from user
-//   socket.on("disconnect", () => {
-//     console.log("disconnected from user");
-//   });
-// });
-
-// server.listen(PORT2, () =>
-//   console.log(`Testing server listening on port ${PORT2}`)
-// );
 
 
-
-// Unknown route handler
-app2.use((req, res) => res.status(404).send('You are in the wrong place! 😡'));
-
-// Global error handler
-app2.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign(defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).send(errorObj.message);
+const webSocketsServerPort = process.env.PORT || 1234;;
+const webSocketServer = require("websocket").server;
+const http = require("http");
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+const wsServer = new webSocketServer({
+  httpServer: server,
 });
 
-app2.listen(PORT2, () => console.log(`Testing server listening on port ${PORT2}`));
+// I'm maintaining all active connections in this object
+const clients = {};
 
-module.exports = app2;
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+wsServer.on('request', function(request) {
+  var userID = getUniqueID();
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
+});
+
+
+
+
+
+
+
+
+
+
+
+// // Unknown route handler
+// app2.use((req, res) => res.status(404).send('You are in the wrong place! 😡'));
+
+// // Global error handler
+// app2.use((err, req, res, next) => {
+//   const defaultErr = {
+//     log: 'Express error handler caught unknown middleware error',
+//     status: 400,
+//     message: { err: 'An error occurred' },
+//   };
+//   const errorObj = Object.assign(defaultErr, err);
+//   console.log(errorObj.log);
+//   return res.status(errorObj.status).send(errorObj.message);
+// });
+
+// app2.listen(PORT2, () => console.log(`Testing server listening on port ${PORT2}`));
+
+// module.exports = app2;
