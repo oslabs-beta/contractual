4935ed70935ce183f5598b97b03391a8df8240cb
George Jeng <George@Georges-MacBook-Pro.local>
2022-04-22 16:00:29 -0700

    documentation preview now shows full contract

--- src/components/DocumentPreview.tsx
+++ src/components/DocumentPreview.tsx
@@ -42,22 +42,43 @@ const DocumentPreview: React.FC<PreviewProps> = ({ currentContract, reqKeys }): 
     // console.log(endpoints);
     return endpoints;
   }
-  const documentation = buildPreview(currentContract, reqKeys);
+  const docObj = buildPreview(currentContract, reqKeys);
   console.log('DOCUMENTATION');
-  console.log(documentation);
+  console.log(docObj);
   // {
   //   endpoint1: [[{key: value}, {key: value}], [{key: value}]],
   //   endpoint2: 
   // }
-
-  return (
-    <div className='px-3 py-3 bg-gray-900'>
+  const documentation = []
+  for (let key in docObj) {
+    let splitKey = key.split('@');
+    const requests = [];
+    const responses = [];
+    for (let request of docObj[key][0]) {
+      requests.push(
+        <>
+          <dt className='text-sm font-medium text-gray-500'>{Object.keys(request)[0]}</dt>
+          <dd className='mt-1 text-sm text-gray-900'>{Object.values(request)[0]}</dd>
+          <br></br>
+        </>
+      )
+    }
+    for (let response of docObj[key][0]) {
+      responses.push(
+        <>
+          <dt className='text-sm font-medium text-gray-500'>{Object.keys(response)[0]}</dt>
+          <dd className='mt-1 text-sm text-gray-900'>{Object.values(response)[0]}</dd>
+          <br></br>
+        </>
+      )
+    }
+    documentation.push(
       <div className='bg-white shadow overflow-hidden sm:rounded-lg mb-3'>
         <div className='px-4 py-5 sm:px-6'>
           <h3 className='text-lg leading-6 font-medium text-gray-900'>
-            /login
+            {splitKey[1]}
           </h3>
-          <p className='mt-1 max-w-2xl text-sm text-gray-500'>POST</p>
+          <p className='mt-1 max-w-2xl text-sm text-gray-500'>{splitKey[0]}</p>
         </div>
         <div className='border-t border-gray-200 px-4 py-5 sm:px-6'>
           <dl className='grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2'>
@@ -67,14 +88,9 @@ const DocumentPreview: React.FC<PreviewProps> = ({ currentContract, reqKeys }): 
               </dt>
               {/* <dd className='mt-1 text-md text-gray-900'>Object</dd> */}
               <div className='sm:col-span-1'>
-                <dt className='text-sm font-medium text-gray-500'>username</dt>
-                <dd className='mt-1 text-sm text-gray-900'>string</dd>
-                <br></br>
-                <dt className='text-sm font-medium text-gray-500'>password</dt>
-                <dd className='mt-1 text-sm text-gray-900'>string</dd>
+                {requests}
               </div>
             </div>
-
 
             <div className='sm:col-span-1'>
               <dt className='text-md font-medium text-gray-500 mb-4'>
@@ -82,12 +98,11 @@ const DocumentPreview: React.FC<PreviewProps> = ({ currentContract, reqKeys }): 
               </dt>
               {/* <dd className='mt-1 text-md text-gray-900'>Object</dd> */}
               <div className='sm:col-span-1'>
-                <dt className='text-sm font-medium text-gray-500'>authStatus</dt>
-                <dd className='mt-1 text-sm text-gray-900'>boolean</dd>
+                {responses}
               </div>
             </div>
 
-            <div className='sm:col-span-2'>
+            {/* <div className='sm:col-span-2'>
               <dt className='text-md font-semibold text-gray-500'>Usage</dt>
               <dd className='mt-1 text-sm text-gray-900'>
                 Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
@@ -96,95 +111,16 @@ const DocumentPreview: React.FC<PreviewProps> = ({ currentContract, reqKeys }): 
                 proident. Irure nostrud pariatur mollit ad adipisicing
                 reprehenderit deserunt qui eu.
               </dd>
-            </div>
+            </div> */}
           </dl>
         </div>
       </div>
+    )
+  }
 
-      <div className="bg-white shadow overflow-hidden sm:rounded-lg mb-3">
-        <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">/feed</h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">POST</p>
-        </div>
-        <div className='border-t border-gray-200 px-4 py-5 sm:px-6'>
-          <dl className='grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2'>
-            <div className='sm:col-span-1'>
-              <dt className='text-md font-medium text-gray-500'>
-                Request Type
-              </dt>
-              <dd className='mt-1 text-md text-gray-900'>Object</dd>
-            </div>
-            <div className='sm:col-span-1'>
-              <dt className='text-md font-medium text-gray-500'>
-                Response Type
-              </dt>
-              <dd className='mt-1 text-md text-gray-900'>Object</dd>
-            </div>
-            <div className='sm:col-span-1'>
-              <dt className='text-sm font-medium text-gray-500'>userId</dt>
-              <dd className='mt-1 text-sm text-gray-900'>number</dd>
-            </div>
-            <div className="sm:col-span-1">
-              <dt className="text-sm font-medium text-gray-500">feedItems</dt>
-              <dd className="mt-1 text-sm text-gray-900">array</dd>
-            </div>
-            <div className='sm:col-span-2'>
-              <dt className='text-sm font-medium text-gray-500'>Usage</dt>
-              <dd className='mt-1 text-sm text-gray-900'>
-                Lorem ipsum ipsum deserunt culpa aute sint do nostrud anim
-                incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-                consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                proident. Irure nostrud pariatur mollit ad adipisicing
-                reprehenderit deserunt qui eu.
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </div>
-
-      <div className="bg-white shadow overflow-hidden sm:rounded-lg mb-3">
-        <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">/addKey</h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">POST</p>
-        </div>
-        <div className='border-t border-gray-200 px-4 py-5 sm:px-6'>
-          <dl className='grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2'>
-            <div className='sm:col-span-1'>
-              <dt className='text-md font-medium text-gray-500'>
-                Request Type
-              </dt>
-              <dd className='mt-1 text-md text-gray-900'>Object</dd>
-            </div>
-            <div className='sm:col-span-1'>
-              <dt className='text-md font-medium text-gray-500'>
-                Response Type
-              </dt>
-              <dd className='mt-1 text-md text-gray-900'>Object</dd>
-            </div>
-            <div className='sm:col-span-1'>
-              <dt className='text-sm font-medium text-gray-500'>contractId</dt>
-              <dd className='mt-1 text-sm text-gray-900'>number</dd>
-              <br></br>
-              <dt className='text-sm font-medium text-gray-500'>kvp</dt>
-              <dd className='mt-1 text-sm text-gray-900'>array</dd>
-            </div>
-            <div className='sm:col-span-1'>
-              <dt className='text-sm font-medium text-gray-500'>keyAdded</dt>
-              <dd className='mt-1 text-sm text-gray-900'>array</dd>
-            </div>
-            <div className='sm:col-span-2'>
-              <dt className='text-sm font-medium text-gray-500'>Usage</dt>
-              <dd className='mt-1 text-sm text-gray-900'>
-                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-                incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-                consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                proident. Irure nostrud pariatur mollit ad adipisicing
-                reprehenderit deserunt qui eu.
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </div>
+  return (
+    <div className='px-3 py-3 bg-gray-900'>
+      {documentation}
     </div>
   );
 }
