a033028bcd1e8153c672011437d9b2ed3c37a2c7
George Jeng <George@Georges-MacBook-Pro.local>
2022-04-22 15:02:39 -0700

    documentation object created, still need to render tsx components based on object

--- src/components/DocumentPreview.tsx
+++ src/components/DocumentPreview.tsx
@@ -1,6 +1,55 @@
 import { PaperClipIcon } from '@heroicons/react/solid';
+import { validateYupSchema } from 'formik';
 
-export default function DocumentPreview() {
+type Contracts = {
+  [key: string]: string
+}
+interface PreviewProps {
+  reqKeys: string[];
+  currentContract: Contracts
+}
+
+const DocumentPreview: React.FC<PreviewProps> = ({ currentContract, reqKeys }): JSX.Element => {
+  // console.log('CURRENT CONTRACT', currentContract);
+  // console.log('CURRENT KEYS', reqKeys);
+  const buildPreview = (contract, reqs) => {
+    const endpoints = {};
+    // Loop through reqKeys of one endpoint
+    for (let req of reqs) {
+      const endpoint = []
+      const reqPairs = [];
+      const resPairs = [];
+      let reqKeys = contract[req];
+      for (let key in reqKeys) {
+        let pair = {};
+        pair[key] = reqKeys[key];
+        reqPairs.push(pair);
+      }
+      endpoint.push(reqPairs);
+      let res = 'Res' + req.slice(3);
+      let resKeys = contract[res];
+      for (let key in resKeys) {
+        resPairs.push({
+          key: resKeys[key] //like this right?
+        })
+        // isn't it already an object?
+        // we are looping through the keys in the object ah right
+      }
+      endpoint.push(resPairs)
+      endpoints[req.slice(4)] = endpoint // i chopped off the beginning again
+    }
+    // console.log('ENDPOINTS');
+    // console.log(endpoints);
+    return endpoints;
+  }
+  const documentation = buildPreview(currentContract, reqKeys);
+  console.log('DOCUMENTATION');
+  console.log(documentation);
+  // {
+  //   endpoint1: [[{key: value}, {key: value}], [{key: value}]],
+  //   endpoint2: 
+  // }
+
   return (
     <div className='px-3 py-3 bg-gray-900'>
       <div className='bg-white shadow overflow-hidden sm:rounded-lg mb-3'>
@@ -139,3 +188,5 @@ export default function DocumentPreview() {
     </div>
   );
 }
+
+export default DocumentPreview;
