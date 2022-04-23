6e5a69c721e4e572f29a4707d5b059206a2d3a52
Yankun Song <yankun.l.song@gmail.com>
2022-04-22 21:32:19 -0500

    adding functions to backend tester
    Co-authored-by: Joe Amos joeamos17@gmail.com
    Co-authored-by: Yankun Song yankun.l.song@gmail.com
    Co-authored-by: George Jeng gjenga@icloud.com
    Co-authored-by: Ernest Leung ernestleung52@gmail.com

--- src/components/BackEndpoint.tsx
+++ src/components/BackEndpoint.tsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
-import { Combobox } from '@headlessui/react'
-import axios from 'axios';
+import { useState } from "react";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import { Combobox } from "@headlessui/react";
+import axios from "axios";
+import { checkInput } from "../express/testing_server/controllers/contractOp";
 
 interface EnumEndpointItem {
-  id: number,
-  name: string
-};
+  id: number;
+  name: string;
+}
 
-const endpoints: EnumEndpointItem[] = [
-  { id: 1, name: '/login' },
-  // More endpoints...
-]
+// const endpoints: EnumEndpointItem[] = [
+//   { id: 1, name: "/login" },
+//   // More endpoints...
+// ];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 // type KeyAndType = {
@@ -24,101 +25,142 @@ function classNames(...classes) {
 type KeyTypeValue = {
   reqKey: string;
   reqValType: string;
-  reqVal: string | number | boolean | any[]
+  reqVal: string | number | boolean | any[];
 };
-type BodyInputs = KeyTypeValue[]
-
+type BodyInputs = KeyTypeValue[];
 
 interface ContractEndpointProps {
-  reqMethod: string,
-  setReqMethod: (e: any) => void,
-  endpoint: string,
-  setEndpoint: (e: any) => void,
-  reqInputs: BodyInputs,
-  resetFields: () => void,
+  reqMethod: string;
+  setReqMethod: (e: any) => void;
+  URLString: string;
+  setURLString: (e: any) => void;
+  endpoint: string;
+  setEndpoint: (e: any) => void;
+  reqInputs: BodyInputs;
+  resetFields: () => void;
+  endpoints: EnumEndpointItem;
 }
 
-export default function BackEndpoint({ reqMethod, setReqMethod, endpoint, setEndpoint, reqInputs, resetFields }) {
-
-  const [query, setQuery] = useState('')
-  const [selectedEndpoint, setSelectedEndpoint] = useState()
+export default function BackEndpoint({
+  reqMethod,
+  setReqMethod,
+  endpoint,
+  setEndpoint,
+  reqInputs,
+  resetFields,
+  URLString,
+  setURLString,
+  endpoints,
+}) {
+  const [query, setQuery] = useState("");
+  const [selectedEndpoint, setSelectedEndpoint] = useState();
 
   const filteredEndpoints =
-    query === ''
+    query === ""
       ? endpoints
       : endpoints.filter((endpoint) => {
-        return endpoint.name.toLowerCase().includes(query.toLowerCase())
-      })
-
+          return endpoint.name.toLowerCase().includes(query.toLowerCase());
+        });
 
   const sendRequest = (
+    URLString: string,
     reqMethod: string,
-    endpoint: string,
-    reqInputs: BodyInputs,
+    endpoint: EnumEndpointItem,
+    reqInputs: BodyInputs
   ): void => {
     // DO A CHECK AGAINS THE DATA CONTRACT HERE
     // name of contract could be argument
     const reqBody = {};
+    const condition = `Res@${reqMethod}@${endpoint.name}`; //endpoint is entire url string
 
     reqInputs.forEach((input) => {
       reqBody[input.reqKey] = input.reqVal;
     });
 
-
     // perform ajax request passing in built request body from above
     // use template literals to send to right endpoints
     // need to perform data contract check
-    if (reqMethod === 'GET') {
-      axios.get(endpoint);
-    }
-    else if (reqMethod === 'POST') {
-      axios.post(endpoint, reqBody)
+
+
+    if (reqMethod === "GET") {
+            console.log("sending a get req");
+
+      axios.get(URLString + endpoint.name);
+    } else if (reqMethod === "POST") {
+      console.log("sending a post req")
+      axios
+        .post(URLString + endpoint.name, reqBody)
+        .then((response) => {
+          // TEST CONDITION HERE
+          console.log(response);
+        })
+        .catch((error) => console.log(error));
+    } else if (reqMethod === "PUT") {
+      axios
+        .put(URLString + endpoint.name, reqBody)
         .then((response) => console.log(response))
-        .catch((error) => console.log(error))
-    }
-    else if (reqMethod === 'PUT') {
-      axios.put(endpoint)
+        .catch((error) => console.log(error));
+    } else if (reqMethod === "PATCH") {
+      axios
+        .patch(URLString + endpoint.name, reqBody)
         .then((response) => console.log(response))
-        .catch((error) => console.log(error))
-    }
-    else if (reqMethod === 'PATCH') {
-      axios.patch(endpoint)
+        .catch((error) => console.log(error));
+    } else if (reqMethod === "DELETE") {
+      axios
+        .delete(URLString + endpoint.name, reqBody)
         .then((response) => console.log(response))
-        .catch((error) => console.log(error))
-    }
-    else if (reqMethod === 'DELETE') {
-      axios.delete(endpoint)
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error))
+        .catch((error) => console.log(error));
     }
 
     //Reset form fields
-
-  }
+  };
   // add new button to reset fields
   // resetFields()
 
   return (
-    <div className='sticky top-16 z-50 bg-gray-900 shadow-lg'>
-      <div className='grid grid-cols-12 gap-1 px-3 py-3'>
-        <div className='col-span-4 sm:col-span-2'>
+    <div className="sticky top-16 z-50 bg-gray-900 shadow-lg">
+      <div className="grid grid-cols-12 gap-1 px-3 py-3">
+        <div className="col-span-4 sm:col-span-2">
           <div>
             <select
               id="reqMethod"
               name="reqMethod"
+              onChange={(e) => {
+                setReqMethod(e);
+              }}
               className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
               defaultValue="GET"
             >
-              <option value="GET" >GET</option>
-              <option value="POST" >POST</option>
-              <option value="PUT" >PUT</option>
-              <option value="PATCH" >PATCH</option>
-              <option value="DELETE" >DELETE</option>
+              <option value="GET">GET</option>
+              <option value="POST">POST</option>
+              <option value="PUT">PUT</option>
+              <option value="PATCH">PATCH</option>
+              <option value="DELETE">DELETE</option>
             </select>
           </div>
         </div>
-        <div className='col-span-8 sm:col-span-7'>
-          <Combobox as="div" value={selectedEndpoint} onChange={setSelectedEndpoint}>
+        <div className="col-span-4 sm:col-span-3">
+          <div>
+            <div className="mt-1">
+              <input
+                type="text"
+                name="domain"
+                id="domain"
+                value={URLString}
+                onChange={(e) => setURLString(e)}
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                placeholder="https://domain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-4 sm:col-span-4">
+          <Combobox
+            as="div"
+            value={selectedEndpoint}
+            onChange={setSelectedEndpoint}
+          >
             <div className="relative mt-1">
               <Combobox.Input
                 className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
@@ -126,7 +168,10 @@ export default function BackEndpoint({ reqMethod, setReqMethod, endpoint, setEnd
                 displayValue={(endpoint: EnumEndpointItem) => endpoint.name}
               />
               <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-                <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                <SelectorIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
               </Combobox.Button>
 
               {filteredEndpoints.length > 0 && (
@@ -137,23 +182,33 @@ export default function BackEndpoint({ reqMethod, setReqMethod, endpoint, setEnd
                       value={endpoint}
                       className={({ active }) =>
                         classNames(
-                          'relative cursor-default select-none py-2 pl-3 pr-9',
-                          active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                          "relative cursor-default select-none py-2 pl-3 pr-9",
+                          active ? "bg-indigo-600 text-white" : "text-gray-900"
                         )
                       }
                     >
                       {({ active, selected }) => (
                         <>
-                          <span className={classNames('block truncate', selected && 'font-semibold')}>{endpoint.name}</span>
+                          <span
+                            className={classNames(
+                              "block truncate",
+                              selected && "font-semibold"
+                            )}
+                          >
+                            {endpoint.name}
+                          </span>
 
                           {selected && (
                             <span
                               className={classNames(
-                                'absolute inset-y-0 right-0 flex items-center pr-4',
-                                active ? 'text-white' : 'text-indigo-600'
+                                "absolute inset-y-0 right-0 flex items-center pr-4",
+                                active ? "text-white" : "text-indigo-600"
                               )}
                             >
-                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
                             </span>
                           )}
                         </>
@@ -165,12 +220,12 @@ export default function BackEndpoint({ reqMethod, setReqMethod, endpoint, setEnd
             </div>
           </Combobox>
         </div>
-        <div className='col-span-12 sm:col-span-3'>
+        <div className="col-span-12 sm:col-span-3">
           <button
             type="button"
             className="items-center text-center h-[38px] w-full mt-1 px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             onClick={() => {
-              sendRequest(reqMethod, endpoint, reqInputs);
+              sendRequest(URLString, reqMethod, selectedEndpoint, reqInputs);
             }}
           >
             Send Request
@@ -178,5 +233,5 @@ export default function BackEndpoint({ reqMethod, setReqMethod, endpoint, setEnd
         </div>
       </div>
     </div>
-  )
+  );
 }
