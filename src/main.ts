8f9deba06ae1e74dfce5528c7d04ff17128613e2
Ernest Leung <ernestleung52@gmail.com>
2022-04-26 19:23:18 -0700

    Added splash page before loading main app. Modified webpack to include the additional splash.html file. Designed animated splash page in App.css file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,35 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+function createSplashWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 200,
+    backgroundColor: '#6e707e',
+    frame: false,
+    transparent: true,
+    webPreferences: {
+      //preload: path.join(__dirname, "./preload.js"),
+      nodeIntegration: true,
+      // worldSafeExecuteJavascript: true,
+      contextIsolation: false,
+      devTools: false,
+    },
+  });
+
+  win.loadFile(`${__dirname}/splash.html`);
+
+  return win;
+}
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     minWidth: 600,
     backgroundColor: '#111827',
+    // No overlapping with splash page
+    show: false,
     webPreferences: {
       //preload: path.join(__dirname, "./preload.js"),
       nodeIntegration: true,
@@ -20,33 +43,25 @@ function createWindow() {
 
   // Open the DevTools.
   win.webContents.openDevTools();
+
+  return win;
 }
 
-// function createSplashWindow() {
-//   const win = new BrowserWindow({
-//     width: 400,
-//     height: 200,
-//     backgroundColor: '#6e707e',
-//     frame: false,
-//     transparent: true,
-//     webPreferences: {
-//       //preload: path.join(__dirname, "./preload.js"),
-//       nodeIntegration: true,
-//       // worldSafeExecuteJavascript: true,
-//       contextIsolation: false,
-//       devTools: false,
-//     },
-//   });
-
-//   win.loadFile(`${__dirname}/splash.html`);
-// }
-
 app.whenReady().then(() => {
-  createWindow();
+  const mainApp = createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  const splash = createSplashWindow();
+
+  mainApp.once('ready-to-show', () => {
+    setTimeout(() => {
+      splash.destroy();
+      mainApp.show();
+    }, 2000);
   });
+
+  // app.on('activate', () => {
+  //   if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  // });
 });
 
 app.on('window-all-closed', () => {
