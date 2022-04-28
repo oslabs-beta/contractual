0ed43945015b3fd4ac25653b12bb94ecb6343f56
Yankun Song <yankun.l.song@gmail.com>
2022-04-27 21:05:31 -0500

    added the sql commands to create tables needed

---
+++ build.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR() NOT NULL UNIQUE,
+    name VARCHAR() NOT NULL,
+    password VARCHAR() NOT NULL
+)
+
+CREATE TABLE contracts(
+    contract_id SERIAL PRIMARY KEY,
+    title VARCHAR() NOT NULL,
+    content VARCHAR() NOT NULL,
+    token VARCHAR() NOT NULL,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+)
+
+CREATE TABLE users_contracts(
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    contract_id INTEGER,
+    FOREIGN KEY (contract_id) REFERENCES contracts(contract_id),
+    permission BOOLEAN
+)
