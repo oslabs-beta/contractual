680adce87140b104cb40b166c969910cd832428d
Yankun Song <yankun.l.song@gmail.com>
2022-04-29 18:28:57 -0500

    Update dbModel.js

--- src/express/testing_server/models/dbModel.js
+++ src/express/testing_server/models/dbModel.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 // Link to ElephantSQL DB
 const PG_URI =
-  "postgres://prtanubs:dWU6razsSRkatrpNGRK4BSwE3h5DvPoA@heffalump.db.elephantsql.com/prtanubs";
+  "";
 
 // Establish connection to DB
 const pool = new Pool({
